f56960dbdc291c99457607823f01306e2179e0ce
mahmutkayadelen <mahmutkyd@gmail.com>
2021-02-16 23:48:57 +0300

    Shared modülünde heryerde kullanmak için confirm coponent oluşturudur. slide barda banka modülü kaldırıldı

--- src/webui/src/app/pages/project/project.component.ts
+++ src/webui/src/app/pages/project/project.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
 import {ProjectService} from "../../services/shared/project.service";
 import {Page} from "../../common/page";
-import {Project} from "../../common/project.model";
 import {BsModalRef, BsModalService} from "ngx-bootstrap";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ConfirmationComponent} from "../../shared/confirmation/confirmation.component";
 
 
 @Component({
@@ -59,6 +59,26 @@ get f(){return this.projectForm.controls}
         this.page.totalElements=pagedData.totalElements;
         this.rows = pagedData.content;
 
+      }
+    );
+  }
+
+   showDeleteConfirmation(){
+    const modal = this.modalService.show(ConfirmationComponent);
+    (<ConfirmationComponent>modal.content).showConfirmation('test heade content','test body content'
+
+    );
+    (<ConfirmationComponent>modal.content).onClose.subscribe(
+      result =>{
+        if(result===true)
+        {
+          console.log("Evet tıklandı")
+        }
+        else if(result ===false)
+        {
+          console.log("Hayır tıklandı")
+
+        }
       }
     );
   }
--- src/webui/src/app/shared/shared.module.ts
+++ src/webui/src/app/shared/shared.module.ts
@@ -1,13 +1,31 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {TranslateModule} from "@ngx-translate/core";
-import {ModalModule} from "ngx-bootstrap";
-import {ReactiveFormsModule} from "@angular/forms";
+import {BsModalRef, ModalModule} from "ngx-bootstrap";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ConfirmationComponent} from './confirmation/confirmation.component';
+import {CommonModule} from "@angular/common";
+import {HttpClient} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+export const createTranslateLoader = (http: HttpClient) => {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 
 @NgModule({
- exports:[
-   TranslateModule,
-   ModalModule,
-   ReactiveFormsModule
- ]
+  exports: [
+    TranslateModule,
+    ModalModule,
+    ReactiveFormsModule,
+    ConfirmationComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ModalModule.forRoot()
+  ],
+  providers :[BsModalRef],
+  entryComponents:[ConfirmationComponent],
+  declarations: [ConfirmationComponent]
 })
-export class SharedModule { }
+export class SharedModule {
+}
